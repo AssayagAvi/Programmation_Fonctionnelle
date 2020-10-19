7ef30e34a0b556f78e9dd4af04b37637f8cb49f6
Avi Assayag <avi_assayag22@hotmail.fr>
2020-10-19 17:22:37 +0200

    Exercice 2

---
+++ src/main/java/TD1/App.java
@@ -0,0 +1,17 @@
+package TD1;
+
+public class App {
+    public static void question2() {
+        Paire<Integer, String> a1 = new Paire<>(2, "Deux");
+        System.out.println(a1);
+        Paire<Double, String> a2 = a1.changeFst(1.0);
+        System.out.println(a2);
+        Paire<Double, Paire<Integer,String>> a3 = a2.changeSnd(a1);
+        System.out.println(a3);
+    }
+
+    public static void main(String[] args) {
+      question2();
+
+    }
+}
--- src/main/java/TD1/Paire.java
+++ src/main/java/TD1/Paire.java
@@ -1,4 +1,32 @@
 package TD1;
 
-public class Paire {
+public class Paire <T , U> {
+    private T fst;
+    private U snd;
+
+    public Paire(T fst, U snd) {
+        this.fst = fst;
+        this.snd = snd;
+    }
+
+    public T fst(){
+        return fst;
+}
+
+    public U snd(){
+        return snd;
+    }
+
+    public <V> Paire <V,U> changeFst(V val){
+        return new Paire<>(val,snd);
+    }
+
+    public <V> Paire <T,V> changeSnd(V val){
+        return new Paire<>(fst,val);
+    }
+    @Override
+    public String toString(){
+        return String.format("(%s, %s) :: Paire [%s, %s]", fst,snd,fst.getClass().getSimpleName(),snd.getClass().getSimpleName());
+    }
+
 }
